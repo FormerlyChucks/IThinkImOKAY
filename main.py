d4787e9baa63999013d3b1aa61f9474c6225c6f0
dio <61555147+Mr-Steal-Your-Script@users.noreply.github.com>
2020-10-06 18:10:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from github import Github
 token = ''
 repo_file = ''
 file1 = ''
+message = ''
+content = ''
 g = Github(token)
 
 while True:
     try:
         repo = g.get_repo(repo_file)
         file = repo.get_contents(file1, ref="main")
-        repo.update_file(file1, "+1 commit <3", "This is just a test!", file.sha)
+        repo.update_file(file1, message, content, file.sha)
         time.sleep(5)
     except Exception as e:
         print(e)

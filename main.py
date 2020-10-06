23dd47eba63f1206b7f69b3a0158dcd372c7f3dc
dio <61555147+Mr-Steal-Your-Script@users.noreply.github.com>
2020-10-06 18:09:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import time
 from github import Github
 
 token = ''
+repo_file = ''
 
 g = Github(token)
 
 while True:
     try:
-        repo = g.get_repo('')
+        repo = g.get_repo(repo_file)
         file = repo.get_contents('', ref="main")
         repo.update_file('', "+1 commit <3", "This is just a test!", file.sha)
         time.sleep(5)

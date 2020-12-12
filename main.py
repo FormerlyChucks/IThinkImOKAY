8b31c2a06c1709713741294a3b9a9e42c4bb9930
dio <61555147+Mr-Steal-Your-Script@users.noreply.github.com>
2020-12-11 21:44:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import time, github, traceback
+import time, github, traceback #packages to use
 
-g = github.Github('<github_api_token>')
+g = github.Github('<github_api_token>') #github api token
 
-while True:
-    try:
-        repo = g.get_repo('<user/repo>')
-        file = repo.get_contents('<file_to_update>', ref="main")
-        repo.update_file('<file_to_update>', '<commit_message>', '<content_to_write_to_file>', file.sha)
-        time.sleep(5)
-    except Exception:
-        print(traceback.format_exc())
-        time.sleep(600)
+while True: #while loop, keeps code running
+    try: #try the following code:
+        repo = g.get_repo('<user/repo>') #github repo
+        file = repo.get_contents('<file_to_update>', ref="main") #file to update, branch
+        repo.update_file('<file_to_update>', '<commit_message>', '<content_to_write_to_file>', file.sha) #update file, commit message, write some text
+        time.sleep(1) #sleep for 1 second
+    except Exception: #if any errors, what do?
+        print(traceback.format_exc()) #print full traceback
+        time.sleep(600) #sleep for 10 minutes

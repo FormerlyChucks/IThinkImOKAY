8168efc1df2cad3ced151657156db52e1c944ec1
dio <61555147+Mr-Steal-Your-Script@users.noreply.github.com>
2020-12-11 21:41:43 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
-import time
-from github import Github
+import time, github, traceback
 
-token = ''
-file1 = ''
-message = ''
-content = ''
-repo_file = ''
-g = Github(token)
+g = github.Github('<github_api_token>')
 
 while True:
     try:
-        repo = g.get_repo(repo_file)
-        file = repo.get_contents(file1, ref="main")
-        repo.update_file(file1, message, content, file.sha)
+        repo = g.get_repo('<user/repo>')
+        file = repo.get_contents('<file_to_update>', ref="main")
+        repo.update_file('<file_to_update>', '<commit_message>', '<content_to_write_to_file>', file.sha)
         time.sleep(5)
-    except Exception as e:
-        print(e)
-        time.sleep(60)
+    except Exception:
+        print(traceback.format_exc())
+        time.sleep(600)

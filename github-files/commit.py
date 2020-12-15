290968e5b95d019b414daa17c638291cce070572
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2020-12-15 16:29:16 -0600

    Create commit.py

---
+++ github-files/commit.py
@@ -0,0 +1,13 @@
+import time, github, traceback #packages to use
+
+g = github.Github('<github_api_token>') #github api token
+
+while True: #while loop, keeps code running
+    try: #try the following code:
+        repo = g.get_repo('<user/repo>') #github repo
+        file = repo.get_contents('<file_to_update>', ref="main") #file to update, branch
+        repo.update_file('<file_to_update>', '<commit_message>', '<content_to_write_to_file>', file.sha) #update file, commit message, write some text
+        time.sleep(3) #sleep for 3 seconds
+    except Exception: #if any errors, what do?
+        print(traceback.format_exc()) #print full traceback
+        time.sleep(600) #sleep for 10 minutes

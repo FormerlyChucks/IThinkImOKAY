c17d1430b6b8fd9ce50f7aa521e1953f3f46644f
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2021-07-05 21:59:59 -0500

    Create unfollow-gh.js

---
+++ code/unfollow-gh.js
@@ -0,0 +1,6 @@
+//go to https://github.com/YOURUSERNAME?tab=following and paste the following into your console
+var divs = document.querySelectorAll('.btn.btn-sm');
+for (i = 0; i < divs.length; ++i) {
+  divs[i].click();
+};
+location.reload();
